56474f52d4d3272917d6df28eed4c8e8cd87d5ac
Alexander Forselius <drsounds@gmail.com>
2022-01-11 17:40:26 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,6 +7,9 @@ let package = Package(
         platforms: [
 	   .iOS(SupportedPlatform.IOSVersion.v11)
         ],
+        products: [
+          .library(name: "FolioReaderKit", targets: ["FolioReaderKit"])
+        ],
 	dependencies: [
 		.package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.1.0"),
 		.package(url: "https://github.com/cxa/MenuItemKit.git", from: "3.0.0"),
@@ -16,9 +19,6 @@ let package = Package(
 		.package(url: "https://github.com/fantim/JSQWebViewController.git", from: "6.1.0"),
 		.package(url: "https://github.com/realm/realm-cocoa.git", from: "3.1.0")
 	],
-        products: [
-          .library(name: "FolioReaderKit", targets: ["FolioReaderKit"])
-        ],
         targets: [
           .target(
              name: "FolioReaderKit",
