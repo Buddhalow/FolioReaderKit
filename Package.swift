93672ac8f40f91f3ce22059becd98a8e5c5c5f4b
Alexander Forselius <drsounds@gmail.com>
2022-01-11 17:36:57 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,15 @@ import PackageDescription
 
 let package = Package(
 	name: "FolioReaderKit",
+	dependencies: [
+		.package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.1.0"),
+		.package(url: "https://github.com/cxa/MenuItemKit.git", from: "3.0.0"),
+		.package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", from: "0.6.0"),
+		.package(url: "https://github.com/tadija/AEXML.git", from: "4.2.0"),
+		.package(url: "https://github.com/ArtSabintsev/FontBlaster.git", from: "4.0.0"),
+		.package(url: "https://github.com/fantim/JSQWebViewController.git", from: "6.1.0"),
+		.package(url: "https://github.com/realm/realm-cocoa.git", from: "3.1.0")
+	],
         platforms: [
 	   .iOS(SupportedPlatform.IOSVersion.v11)
         ],
@@ -21,14 +30,5 @@ let package = Package(
              dependencies: ["FolioReaderKit"],
              path: "Tests"
           )
-        ],
-	dependencies: [
-		.package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.1.0"),
-		.package(url: "https://github.com/cxa/MenuItemKit.git", from: "3.0.0"),
-		.package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", from: "0.6.0"),
-		.package(url: "https://github.com/tadija/AEXML.git", from: "4.2.0"),
-		.package(url: "https://github.com/ArtSabintsev/FontBlaster.git", from: "4.0.0"),
-		.package(url: "https://github.com/fantim/JSQWebViewController.git", from: "6.1.0"),
-		.package(url: "https://github.com/realm/realm-cocoa.git", from: "3.1.0")
-	]
+        ]
 )
